2b24254e2d2657dbd44197c98d217fdb91a8908c
Bilal Asghar <bilalasghar.work@gmail.com>
2023-02-07 11:53:06 +0500

    add root level get

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+app.get("/" ,( req,res) => {
+res.status(200);
+res.send("Wellcome to rool level of server")
+})
+
+app.get("/hello" , (req,res) => {
+    res.set('Content-Type', 'text/html')
+ res.status(200).send("Hello, world!")
+
+})
+
 app.listen(PORT , (error) =>{
     if (!error)
     console.log("server is successfully listening and app is on port " + PORT)
